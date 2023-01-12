4cb6c9472e4854043ae0d00e36a2fc608b3131ba
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2023-01-12 08:37:30 +0100

    Terminado

--- CopiaInserccion-Tema5/src/ejercicios/Ejercicio3.java
+++ CopiaInserccion-Tema5/src/ejercicios/Ejercicio3.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
 
+	static boolean esPrimos(int numero) {
+		boolean estado = false;
+		for (int i = 2; i < numero; i++) {
+			if (numero % i == 0) {
+				break;
+			}
+
+			else {
+				estado = true;
+			}
+		}
+		return estado;
+	}
+
 	/**
 	 * Realiza un programa que pida 10 números por teclado y que los almacene en un
 	 * array. Seguidamente, el programa pasará los números primos a las primeras
@@ -15,7 +29,37 @@ public class Ejercicio3 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Scanner read = new Scanner (System.in);
+
+		Scanner read = new Scanner(System.in);
+
+		int tabla[] = new int[10];
+		int tabla2[] = new int[10];
+
+		System.out.println("Introduce 10 números: ");
+		
+		for (int i = 0; i < tabla.length; i++) {
+			tabla[i] = read.nextInt();
+		}
+
+		System.out.println(Arrays.toString(tabla));
+
+		int incremento = 0;
+		for (int primos : tabla) {
+			if (esPrimos(primos) == true) {
+				tabla2[incremento] = primos;
+				incremento++;
+			}
+		}
+
+		for (int noPrimos : tabla) {
+			if (esPrimos(noPrimos) == false) {
+				tabla2[incremento] = noPrimos;
+				incremento++;
+			}
+
+		}
+		
+		System.out.println(Arrays.toString(tabla2));
 	}
 
 }
